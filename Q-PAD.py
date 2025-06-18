682ee59615ab4964f35c02ef9bce2080dc9a0e39
NemophilistTabar <creeprz08@icloud.com>
2025-06-18 20:03:29 +1000

    vcvvsfd

---
+++ Q-PAD.py
@@ -0,0 +1,28 @@
+# import libraries
+import customtkinter
+from customtkinter import CTk, set_appearance_mode, set_default_color_theme
+import PIL
+from PIL import Image, ImageTk
+import tkinter as tk
+
+# Set appearance parameters
+set_appearance_mode("system")
+set_default_color_theme("blue")
+
+# establish aplication
+class QPAD(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Q-PAD")
+        self.geometry("400x400")
+
+
+        # app widgets
+        self.button = customtkinter.CTkButton(self, command=self.button_click)
+
+    # app methods
+    def button_click(self):
+        print("button click")
+
+app = QPAD()
+app.mainloop()
