7a51f3f409a4027eeb794ca8c5216a7e3a7da4aa
Nem0phil1st <148019583+NemophilistTabar@users.noreply.github.com>
2025-06-25 15:12:01 +1000

    The True Pinicle of Tech

--- Q-PAD.py
+++ Q-PAD.py
@@ -14,13 +14,34 @@ class QPAD(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title("Q-PAD")
-        self.geometry("400x400")
+        self.geometry("1200x700")
 
-        # app widgets
-        self.button = customtkinter.CTkButton(self, command=self.button_click)
-        self.button.grid(row=0, column=0, padx=20, pady=10)
+        # configure grid layout
+        for i in range(15):
+            self.grid_columnconfigure(i, weight=1)
+            # self.grid_rowconfigure(3, weight=1)
 
-    # app methods
+        # frame configeration
+        self.button_frame = customtkinter.CTkFrame(self)
+        self.button_frame.grid(row=2, column=3, rowspan=3, padx=20, pady=10)
+
+        # images repository
+        image = Image.open("Images/bcacu.png")
+        resized_image = image.resize((150, 150))
+        self.logo = ImageTk.PhotoImage(resized_image)
+
+        # app buttons
+        self.databasebutton = customtkinter.CTkButton(self.button_frame, command=self.button_click, text="View Database")
+        self.databasebutton.pack(pady=5)
+        self.individualbutton = customtkinter.CTkButton(self.button_frame, command=self.button_click, text="View Cadet Equipment")
+        self.individualbutton.pack(pady=5)
+        self.reportbutton = customtkinter.CTkButton(self.button_frame, command=self.button_click, text="Generate Report")
+        self.reportbutton.pack(pady=5)
+
+        self.bcacu = customtkinter.CTkLabel(self, image=self.logo, text="")
+        self.bcacu.grid(row=15, column=1, padx=20, pady=20)
+
+    # app functions
     def button_click(self):
         print("button click")
 
