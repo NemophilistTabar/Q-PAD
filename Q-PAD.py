13553164ef035c1d77156e082930379eef2078a7
Nem0phil1st <148019583+NemophilistTabar@users.noreply.github.com>
2025-06-26 15:16:13 +1000

    small modifications and tweaks

--- Q-PAD.py
+++ Q-PAD.py
@@ -17,29 +17,54 @@ class QPAD(customtkinter.CTk):
         self.geometry("1200x700")
 
         # configure grid layout
-        for i in range(15):
+        for i in range(4):
             self.grid_columnconfigure(i, weight=1)
-            # self.grid_rowconfigure(3, weight=1)
+        for i in range(15):
+            self.grid_rowconfigure(i, weight=1)
 
         # frame configeration
         self.button_frame = customtkinter.CTkFrame(self)
-        self.button_frame.grid(row=2, column=3, rowspan=3, padx=20, pady=10)
+        self.button_frame.grid(row=3, column=1, rowspan=3, padx=20, pady=10)
 
         # images repository
         image = Image.open("Images/bcacu.png")
-        resized_image = image.resize((150, 150))
+        resized_image = image.resize((200, 200))
         self.logo = ImageTk.PhotoImage(resized_image)
 
         # app buttons
-        self.databasebutton = customtkinter.CTkButton(self.button_frame, command=self.button_click, text="View Database")
-        self.databasebutton.pack(pady=5)
-        self.individualbutton = customtkinter.CTkButton(self.button_frame, command=self.button_click, text="View Cadet Equipment")
-        self.individualbutton.pack(pady=5)
-        self.reportbutton = customtkinter.CTkButton(self.button_frame, command=self.button_click, text="Generate Report")
-        self.reportbutton.pack(pady=5)
+        button_width = 500
+        button_height = 100
 
+        self.databasebutton = customtkinter.CTkButton(
+            self.button_frame,
+            command=self.button_click,
+            text="View Database",
+            width=button_width,
+            height=button_height)
+        self.databasebutton.pack(pady=20)
+
+        self.individualbutton = customtkinter.CTkButton(
+            self.button_frame,
+            command=self.button_click,
+            text="View Cadet Equipment",
+            width=button_width,
+            height=button_height)
+        self.individualbutton.pack(pady=20)
+
+        self.reportbutton = customtkinter.CTkButton(
+            self.button_frame,
+            command=self.button_click,
+            text="Generate Report",
+            width=button_width,
+            height=button_height)
+        self.reportbutton.pack(pady=20)
+
+        # app labels
         self.bcacu = customtkinter.CTkLabel(self, image=self.logo, text="")
-        self.bcacu.grid(row=15, column=1, padx=20, pady=20)
+        self.bcacu.grid(row=14, column=1, padx=20, pady=10)
+
+        self.titlelabel = customtkinter.CTkLabel(self, text="Welcome to Q-PAD", font=("Arial", 24, "bold"))
+        self.titlelabel.grid(row=0, column=1, pady=20)
 
     # app functions
     def button_click(self):
